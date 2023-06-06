e21ad0be01dd3b3805ec719ebd7259df8a15c6b7
femsworld <femsworld@gmail.com>
2023-06-06 22:49:30 +0300

    Reading the customer.csv file

--- Program.cs
+++ Program.cs
@@ -47,9 +47,9 @@
 class Customer
 {
     public int Id { get; set; }
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
-    public string Email { get; set; }
-    public string Address { get; set; }
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public string? Email { get; set; }
+    public string? Address { get; set; }
 }
 
