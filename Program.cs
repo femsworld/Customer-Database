42f6b9899ab72c75d81713abed8861ac4a734be1
femsworld <femsworld@gmail.com>
2023-06-07 22:04:31 +0300

    Added a new user

--- Program.cs
+++ Program.cs
@@ -9,14 +9,31 @@
         {
             Console.WriteLine($"ID: {customer.Id}");
             Console.WriteLine($"First Name: {customer.FirstName}");
-            Console.WriteLine($"Last Name: {customer.LastName}");
-            Console.WriteLine($"Email: {customer.Email}");
-            Console.WriteLine($"Address: {customer.Address}");
+            // Console.WriteLine($"Last Name: {customer.LastName}");
+            // Console.WriteLine($"Email: {customer.Email}");
+            // Console.WriteLine($"Address: {customer.Address}");
             Console.WriteLine();
         }
+
+        // Example: Add a new customer
+        Customer newCustomer = new Customer
+        {
+            Id = 123,
+            FirstName = "John",
+            LastName = "Doe",
+            Email = "john.doe@example.com",
+            Address = "123 Main St"
+        };
+
+        customers.Add(newCustomer);
+
+        // Write the updated customer list to the CSV file
+        WriteCustomersToCSV(filePath, customers);
     }
+
     static List<Customer> ReadCustomersFromCSV(string filePath)
     {
+        // Existing code for reading customers from the CSV file
         List<Customer> customers = new List<Customer>();
 
         if (File.Exists(filePath))
@@ -43,7 +60,23 @@
 
         return customers;
     }
+
+    static void WriteCustomersToCSV(string filePath, List<Customer> customers)
+    {
+        using (StreamWriter writer = new StreamWriter(filePath))
+        {
+            // Write the header row
+            writer.WriteLine("ID,First Name,Last Name,Email,Address");
+
+            foreach (Customer customer in customers)
+            {
+                // Write the data for each customer
+                writer.WriteLine($"{customer.Id},{customer.FirstName},{customer.LastName},{customer.Email},{customer.Address}");
+            }
+        }
+    }
 }
+
 class Customer
 {
     public int Id { get; set; }
